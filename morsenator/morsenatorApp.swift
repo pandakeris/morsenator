e4972fc9628bc453eee9ec59245a1befa63a5679
Blueguy51 <wantoniusdasilverfang45@gmail.com>
2023-07-31 15:14:05 +0700

    Initial Commit

---
+++ morsenator/morsenatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  morsenatorApp.swift
+//  morsenator
+//
+//  Created by Timothyus Kevin Dewanto on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct morsenatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
